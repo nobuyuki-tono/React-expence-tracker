234135d11db10988e0e3234a0388dc7ba62a20cf
Nobu <rockmusic2090@gmail.com>
2020-02-22 16:41:26 -0800

    Added className for transaction

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const Transaction = () => {
+const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
+
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button className="delete-btn">x</button>
+    </li>
   );
 };
 
--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 
+import Transaction from "./Transaction";
+
 import { GlobalContext } from "../context/GlobalState";
 
 const TransactionList = () => {
@@ -10,10 +12,7 @@ const TransactionList = () => {
       <h3>History</h3>
       <ul className="list">
         {transactions.map(transaction => (
-          <li className="minus">
-            {transaction.text} <span>{transaction.amount}</span>
-            <button className="delete-btn">x</button>
-          </li>
+          <Transaction key={transaction.id} transaction={transaction} />
         ))}
       </ul>
     </>
