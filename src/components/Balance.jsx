bebbad7e4c58d50ddc3d2ed53bdd436d396e2d0c
Nobu <rockmusic2090@gmail.com>
2020-02-22 16:53:31 -0800

    Imported GlobalContext to IncomeExpences.jsx and calculate income and expence

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { GlobalContext } from "../context/GlobalState";
 
 const Balance = () => {
-  const { transactiions } = useContext(GlobalContext);
+  const { transactions } = useContext(GlobalContext);
 
-  const amounts = transaction.map(transaction => transaction.amount);
+  const amounts = transactions.map(transaction => transaction.amount);
   const total = amounts.reduce((acc, amount) => (acc += amount), 0).toFixed(2);
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>$${total}</h1>
+      <h1>${total}</h1>
     </>
   );
 };
