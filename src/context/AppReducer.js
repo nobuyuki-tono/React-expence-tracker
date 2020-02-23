a7991a82e4c94f3123584cf79f1a4f0904291a3a
Nobu <rockmusic2090@gmail.com>
2020-02-22 17:11:51 -0800

    Added type ADD_TRANSACTION to AppReducer

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,11 @@
 export default (state, action) => {
   switch (action.type) {
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: action.payload
+      };
+
     case "DELETE_TRANSACTION":
       return {
         ...state,
