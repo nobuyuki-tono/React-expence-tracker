bf0b97821c3f0d8e24e128238aad777ffd90dbf5
Nobu <rockmusic2090@gmail.com>
2020-02-22 16:45:59 -0800

    Imported GlobalContext to Blance.js and calculate a total amount

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+import { GlobalContext } from "../context/GlobalState";
+
 const Balance = () => {
+  const { transactiions } = useContext(GlobalContext);
+
+  const amounts = transaction.map(transaction => transaction.amount);
+  const total = amounts.reduce((acc, amount) => (acc += amount), 0).toFixed(2);
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>$${total}</h1>
     </>
   );
 };
