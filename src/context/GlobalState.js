d9024e4d74cc0d02d82bedf0ab2b8bfeeadc4151
Nobu <rockmusic2090@gmail.com>
2020-02-22 16:15:31 -0800

    Added GlobalProvider function

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -12,3 +12,14 @@ const initialState = {
 
 // Create context
 export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
