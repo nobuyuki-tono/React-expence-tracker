5bfcf792f06b859ba7282f5bfd3d9859ad291d71
Nobu <rockmusic2090@gmail.com>
2020-02-25 14:34:03 -0800

    Created getTransaction function in transaction controller

---
+++ controllers/transactions.js
@@ -0,0 +1,6 @@
+// @desc Get all transactions
+// @route /api/v1/transactions
+// @acess Public
+exports.getTransactions = (req, res, next) => {
+  res.send("GET Transactions");
+};
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { getTransactions } = require("../controllers/transactions");
 
 router.get("/", (req, res) => {
   res.send("<h1>HEllo World</h1>");
