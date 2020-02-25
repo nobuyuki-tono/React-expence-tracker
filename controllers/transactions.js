46ffa05d8e5f45500f9b96d4c52a7c620c35bf1d
Nobu <rockmusic2090@gmail.com>
2020-02-25 15:55:05 -0800

    Modified getTransactions and addTransaction function to get and add data to mongoDB

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,46 @@
+const Transaction = require("../Model/Transaction");
+
 // @desc Get all transactions
 // @route GET /api/v1/transactions
 // @acess Public
-exports.getTransactions = (req, res, next) => {
-  res.send("GET Transactions");
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error"
+    });
+  }
 };
 
 // @desc Add transactions
 // @route POST /api/v1/transactions
 // @acess Public
-exports.addTransaction = (req, res, next) => {
-  res.send("ADD Transaction");
+exports.addTransaction = async (req, res, next) => {
+  try {
+    const { text, amount } = req.body;
+
+    const transaction = await Transaction.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      data: transaction
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // @desc Delete transactions
 // @route DELETE /api/v1/transactions/:id
 // @acess Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send("DELETE transactions");
 };
